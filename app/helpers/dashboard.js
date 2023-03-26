833252e35a5be0e042b50e8988d96ef17d3ea578
Louis Payette <payette1996@gmail.com>
2023-03-26 09:39:27 -0400

    work on api

--- app/helpers/dashboard.js
+++ app/helpers/dashboard.js
@@ -14,7 +14,7 @@ async function fetchSession() {
 
 async function printSession() {
     const session = await fetchSession();
-    
+
     const section = document.querySelector("#sessionSection");
     Object.keys(session).forEach(key => {
         section.innerHTML += `<strong>${key} : </strong>${session[key]}<br>`;
@@ -22,3 +22,39 @@ async function printSession() {
 }
 
 printSession();
+
+async function fetchBlogs() {
+    const response = await fetch("/blogapi/blogs");
+    if (response.ok) {
+        return await response.json();
+    }
+}
+
+async function printBlogs() {
+    const blogs = await fetchBlogs();
+
+    const section = document.querySelector("#blogsSection");
+    Object.keys(blogs).forEach(key => {
+        section.innerHTML += `<strong>${key} : </strong>${blogs[key]}<br>`;
+    });
+}
+
+printBlogs();
+
+async function fetchPosts() {
+    const response = await fetch("/blogapi/posts");
+    if (response.ok) {
+        return await response.json();
+    }
+}
+
+async function printPosts() {
+    const posts = await fetchPosts();
+
+    const section = document.querySelector("#postsSection");
+    Object.keys(posts).forEach(key => {
+        section.innerHTML += `<strong>${key} : </strong>${posts[key]}<br>`;
+    });
+}
+
+printPosts();
